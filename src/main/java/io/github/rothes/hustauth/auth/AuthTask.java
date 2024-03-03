1e2eadc78fc4339faeb9c9794a7fe9c16ce61f5e
Rothes <449181985@qq.com>
2024-03-03 11:03:11 +0800

    Handle task exceptions

--- src/main/java/io/github/rothes/hustauth/auth/AuthTask.java
+++ src/main/java/io/github/rothes/hustauth/auth/AuthTask.java
@@ -18,24 +18,29 @@ public class AuthTask {
         TimerTask newTask = new TimerTask() {
             @Override
             public void run() {
-                if (AuthHandler.check() == AuthHandler.AuthStatus.LOGGED_IN) {
-                    if (!once) {
-                        HustAuth.log("目前已登入校园网, 终止登入任务.");
-                        stop();
-                    } else if (HustAuth.INS.getConfigManager().getConfigData().verbose) {
-                        HustAuth.log("目前已登入校园网, 等待下次执行...");
+                try {
+                    if (AuthHandler.check() == AuthHandler.AuthStatus.LOGGED_IN) {
+                        if (!once) {
+                            HustAuth.log("目前已登入校园网, 终止登入任务.");
+                            stop();
+                        } else if (HustAuth.INS.getConfigManager().getConfigData().verbose) {
+                            HustAuth.log("目前已登入校园网, 等待下次执行...");
+                        }
+                        return;
                     }
-                    return;
-                }
 
-                HustAuth.log("正在尝试登入校园网...");
-                AuthHandler.Result login = AuthHandler.login();
-                if (login.isSuccess()) {
-                    HustAuth.log("成功登录校园网, 登入任务完成.");
-                } else {
-                    HustAuth.log("登入校园网失败, " + login.getMessage() + "; 登入任务终止.");
+                    HustAuth.log("正在尝试登入校园网...");
+                    AuthHandler.Result login = AuthHandler.login();
+                    if (login.isSuccess()) {
+                        HustAuth.log("成功登录校园网, 登入任务完成.");
+                    } else {
+                        HustAuth.log("登入校园网失败, " + login.getMessage() + "; 登入任务终止.");
+                    }
+                    stop();
+                } catch (Throwable throwable) {
+                    HustAuth.error("致命错误: ", throwable);
+                    stop();
                 }
-                stop();
             }
         };
 
